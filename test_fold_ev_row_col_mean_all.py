63ec5b3bb5d302d267824acc660d9a13e3102cf0
Arif <arif@themascoteers.com>
2023-08-25 05:33:57 +1000

    done

--- test_fold_ev_row_col_mean_all.py
+++ test_fold_ev_row_col_mean_all.py
@@ -14,9 +14,12 @@ if __name__ == "__main__":
 
     for inputs in ["bands", "props_ex_som", "all_ex_som",
                    "bands_elevation","bands_moisture","bands_temp",
-                   "bands_elevation_moisture","bands_moisture_temp","bands_temp_elevation"]:
-        for ag in ["row", "col", "mean"]:
-            for lim in range(5):
+                   "bands_elevation_moisture","bands_moisture_temp","bands_temp_elevation",
+                   ["elevation"],["moisture"],["temp"],
+                   ["elevation","moisture"],["moisture","temp"],["temp","elevation"]
+                   ]:
+        for ag in ["row"]:#, "col", "mean"]:
+            for lim in range(1,5):
                 configs.append({"input": inputs, "scenes": scenes[0:lim+1], "ag":ag})
-    c = FoldEvaluator(configs=configs, prefix="all", folds=10)
+    c = FoldEvaluator(configs=configs, prefix="all2", folds=10, algorithms=["mlr", "rf", "svr", "ann"])
     c.process()
